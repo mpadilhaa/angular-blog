347b8d6ad4bbd48d94576476e74f85e83fcb435b
mpadilhaa <mpadilhaaac@gmail.com>
2023-12-25 14:53:17 -0300

    feat:create global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, BigCardComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    BigCardComponent,
+    SmallCardComponent,
+    MenuTitleComponent,
+    MenuBarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
